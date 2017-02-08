c7a1ffc7049d58eb76946098f366e37b950d4639
JonathanNiehenke <jrniehenke@gmail.com>
2017-02-07 19:54:42 -0800

    Introduce portals with key input

--- Portal.js
+++ Portal.js
@@ -2,6 +2,8 @@ let db = console.log;
 
 function TilePortal() {
     this.__proto__ = new Engine(undefined, "v");
+    this.Portal1 = {"type": "o", "index": undefined, "value": undefined};
+    this.Portal2 = {"type": "O", "index": undefined, "value": undefined};
     this.parseLevelFile = function*(levelFile) {
         let fileLines = levelFile.target.result.split("\n");
         Structure = [];
@@ -68,6 +70,39 @@ function TilePortal() {
         this.Inventory.map[moveTo] = cellTo;
         this.replaceImage(moveTo, cellTo);
     };
+    this.shootPortal = function(Portal) {
+        let Movement = {
+            "^": [-1, 0],
+            "v": [1, 0],
+            "<": [0, -1],
+            ">": [0, 1],
+        }[this.Environment.facing];
+        let targetIndex = [
+            this.Environment.player[0] + Movement[0],
+            this.Environment.player[1] + Movement[1]
+        ];
+        while (this.Environment.cell.hasOwnProperty(targetIndex)) {
+            if (this.Environment.cell[targetIndex] == "#") {
+                this.removePortal(Portal);
+                this.placePortal(Portal, targetIndex);
+                break;
+            }
+            targetIndex = [
+                targetIndex[0] + Movement[0],
+                targetIndex[1] + Movement[1]
+            ];
+        }
+    }
+    this.removePortal = function(Portal) {
+        if (Portal.index) {
+            this.replaceCell(Portal.index, Portal.value);
+        }
+    };
+    this.placePortal = function(Portal, portalIndex) {
+        Portal.index = portalIndex;
+        Portal.value = this.Environment.cell[portalIndex];
+        this.replaceCell(portalIndex, Portal.type);
+    }
     this.__constructTiles = function() {
         let getImg = document.getElementById.bind(document);
         Tile = {
@@ -78,8 +113,51 @@ function TilePortal() {
             "v": {"image": getImg("Down"), "action": undefined},
             "<": {"image": getImg("Left"), "action": undefined},
             ">": {"image": getImg("Right"), "action": undefined},
+            "o": {"image": getImg("Portal1"), "action": undefined},
+            "O": {"image": getImg("Portal2"), "action": undefined},
         };
         return Tile;
+    };
+    this.keyInput = {
+        "movementKeys": {
+            "38": [-1, 0], // Up Arrow
+            "40": [1, 0],  // Down Arrow
+            "37": [0, -1], // Left Arrow
+            "39": [0, 1],  // Right Arrow
+            "87": [-1, 0], // W Key (Up)
+            "83": [1, 0],  // S Key (Down)
+            "65": [0, -1], // A Key (Left)
+            "68": [0, 1],  // D Key (Right)
+            "73": [-1, 0], // I key (Up)
+            "75": [1, 0],  // K Key (Down)
+            "74": [0, -1], // J key (Left)
+            "76": [0, 1],  // L key (Right)
+        },
+        "portalKeys": {
+            "81": this.Portal1,  // Q Key
+            "69": this.Portal2,  // E Key
+            "85": this.Portal1,  // U Key
+            "79": this.Portal2,  // O Key
+            "17": this.Portal1,  // Ctrl Key
+            "16": this.Portal2,  // Shift Key
+        },
+        "handle": function(keyEvent) {
+            let Movement = this.keyInput.movementKeys[keyEvent.keyCode];
+            let Portal = this.keyInput.portalKeys[keyEvent.keyCode];
+            if (Movement) {
+                let moveTo = [
+                    this.Environment.player[0] + Movement[0],
+                    this.Environment.player[1] + Movement[1]];
+                let cellTo = this.Environment.cell[moveTo];
+                let cellAction = this.Tile[cellTo].action;
+                if (moveTo in this.Environment.cell && cellAction) {
+                    cellAction.call(this, moveTo, cellTo, Movement);
+                }
+            }
+            else if (Portal) {
+                this.shootPortal(Portal);
+            }
+        },
     };
     this.Tile = this.__constructTiles();
 }
